ab6f0349849d65083980b74153e9df8f4e2a7695
JordiGonzalez2021 <jordi.gonzalez.29@lacetania.cat>
2021-11-25 11:44:59 +0100

    Error al guardar (Rectificació)

--- Comunicació Sèrie Bàsica/Exercici_003/Exercici_003.ino
+++ Comunicació Sèrie Bàsica/Exercici_003/Exercici_003.ino
@@ -1,4 +1,4 @@
-/********************************************************************************
+/*********************************************************************************
 **                                                                              **
 **                              Hello World                                     **
 **                                                                              **
@@ -12,17 +12,14 @@
 //********** Setup ***************************************************************
 void setup()                // run once, when the sketch starts
 {
-  Serial.begin(9600);
-  Serial.println("Escull el numero de la operacio que vols realitzar?");  
-  Serial.println("  1.Comprovar numero de targeta de credit");
-  Serial.println("  2.Comprovar numero de compte bancari");
-  Serial.println("  3.Buscar un digit perdut de targeta de credit");
+  Serial.begin(9600);       // set up Serial library at 9600 bps
 }
 
 //********* Loop *****************************************************************
 void loop()                 // run over and over again
 {
- 
+  Serial.println("Hello world!");  
+  delay(1000);
 }
 
 //********* Funcions *************************************************************
