67adf9bd31f656b8d2aaffe03e7b750481aa16f9
JordiGonzalez2021 <jordi.gonzalez.29@lacetania.cat>
2021-12-10 11:32:58 +0100

    First upload

---
+++ 004_Estructures de Control de Repetició for & while/Exercici_006/Exercici_006.ino
@@ -0,0 +1,30 @@
+/****************************************************************
+**                                                             **
+**                            TÍTOL:                           **
+**                  Nom represenatiu del programa              **
+**                                                             **
+** NOM: Nom de l'Alumne                       DATA: xx/xx/20xx **
+****************************************************************/
+//************************** INCLUDE ****************************
+
+
+//************************* VARIABLES ***************************
+int comptar = 11;
+int i = 0;
+//*************************** SETUP *****************************
+void setup()               // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+  Serial.print("Ara comptare de 0 a ");
+  Serial.println(comptar);  
+  while(i <= comptar)
+  {
+    Serial.print(i);
+    Serial.print("-");
+    i++;
+  } 
+}
+void loop()   // we need this to be here even though its empty
+{
+}
+//************************* FUNCIONS ****************************
