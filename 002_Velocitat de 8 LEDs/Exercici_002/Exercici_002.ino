5da1954395fdf4efa6e2591ba315ac0e300e1cad
JordiGonzalez2021 <jordi.gonzalez.29@lacetania.cat>
2022-01-17 15:17:26 +0100

    First upload

---
+++ 002_Velocitat de 8 LEDs/Exercici_002/Exercici_002.ino
@@ -0,0 +1,38 @@
+/****************************************************************
+**                                                             **
+**                            TÍTOL:                           **
+**                  Nom represenatiu del programa              **
+**                                                             **
+** NOM: Nom de l'Alumne                       DATA: xx/xx/20xx **
+****************************************************************/
+//************************** INCLUDE ****************************
+
+
+//************************* VARIABLES ***************************
+const byte led0 = 5;          // donar nom al pin 5 de l’Arduino
+const byte led1 = 6;          // donar nom al pin 6 de l’Arduino
+const byte led2 = 7;          // donar nom al pin 7 de l’Arduino
+const byte led3 = 8;          // donar nom al pin 8 de l’Arduino
+const byte led4 = 9;          // donar nom al pin 9 de l’Arduino
+const byte led5 = 10;         // donar nom al pin 10 de l’Arduino
+const byte led6 = 11;         // donar nom al pin 11 de l’Arduino
+const byte led7 = 12;         // donar nom al pin 12 de l’Arduino
+const byte pot0 = A0;         // donar nom al pin A0 de l’Arduino
+
+unsigned long velocitat = 20; // velocitat de l'acció en ms
+int valPot0;                  // guardar valor del potenciometre
+
+//*************************** SETUP *****************************
+
+void setup() 
+{
+  
+}
+
+//*************************** LOOP ******************************
+void loop() 
+{
+  
+}
+
+//************************* FUNCIONS ****************************
