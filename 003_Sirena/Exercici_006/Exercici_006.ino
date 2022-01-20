11383f397468f7161e6bd3f242b2b4aeac57fd1e
JordiGonzalez2021 <jordi.gonzalez.29@lacetania.cat>
2022-01-20 22:51:45 +0100

    First upload

---
+++ 003_Sirena/Exercici_006/Exercici_006.ino
@@ -0,0 +1,42 @@
+/****************************************************************
+**                                                             **
+**                            TÍTOL:                           **
+**                  Nom represenatiu del programa              **
+**                                                             **
+** NOM: Nom de l'Alumne                       DATA: xx/xx/20xx **
+****************************************************************/
+//************************** INCLUDE ****************************
+
+
+//************************* VARIABLES ***************************
+const byte xiulet = 9;
+const byte pot0 = A0;
+const byte pot1 = A1;
+int valPot0, valPot1;
+
+//*************************** SETUP *****************************
+
+void setup() 
+{
+  pinMode(xiulet, OUTPUT);
+  pinMode(pot0, INPUT);
+  pinMode(pot1, INPUT);
+  Serial.begin(9600);
+}
+
+//*************************** LOOP ******************************
+void loop() 
+{
+  valPot0 = analogRead(pot0)+50;
+  valPot1 = analogRead(pot1)+50;
+  
+  Serial.print("valPot0 = ");
+  Serial.println(valPot0);
+  Serial.print("valPot1 = ");
+  Serial.println(valPot1);
+  
+  tone(xiulet, valPot1, valPot0);
+  delay(2000);
+}
+
+//************************* FUNCIONS ****************************
